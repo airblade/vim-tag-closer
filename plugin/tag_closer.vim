airblade/vim-tag-closer

commit 974635a0870a93bf325143c4e48224442638fc5e

Handle tag names containing numbers
Reference: https://html.spec.whatwg.org/#valid-custom-element-name

Fixes #1.

plugin/tag_closer.vim
@@ -8,7 +8,7 @@ let s:void_elements = ['area', 'area', 'base', 'br', 'col', 'embed',
       \ 'hr', 'img', 'input', 'link', 'meta', 'source', 'track', 'wbr']
 
 function! s:tag_name(tag)
-  return matchstr(a:tag, '\v^\</?\zs\a+(\-\a+)?')
+  return matchstr(a:tag, '\v^\</?\zs\a\w+(\-\a\w+)?')
 endfunction
 
 function! s:is_void_element(tag)

test/test_tag_closer.vim
@@ -23,6 +23,9 @@ function! Test_tag_name()
   call assert_equal('foo-bar', call(TagName, ['<foo-bar id="bar">']))
   call assert_equal('foo-bar', call(TagName, ['</foo-bar>']))
   call assert_equal('foo-bar', call(TagName, ['<foo-bar />']))
+
+  call assert_equal('h1', call(TagName, ['<h1>']))
+  call assert_equal('x1foo-y2bar', call(TagName, ['<x1foo-y2bar>']))
 endfunction
 
 

commit 2f33c8e92528d246d487cc81c8c8bddda6a61b39

Fix tag regexp for tags of one letter
This bug was introduced by 974635a0870a93bf325143c4e48224442638fc5e.

plugin/tag_closer.vim
@@ -8,7 +8,7 @@ let s:void_elements = ['area', 'area', 'base', 'br', 'col', 'embed',
       \ 'hr', 'img', 'input', 'link', 'meta', 'source', 'track', 'wbr']
 
 function! s:tag_name(tag)
-  return matchstr(a:tag, '\v^\</?\zs\a\w+(\-\a\w+)?')
+  return matchstr(a:tag, '\v^\</?\zs\a\w*(\-\a\w*)?')
 endfunction
 
 function! s:is_void_element(tag)

test/test_tag_closer.vim
@@ -24,6 +24,7 @@ function! Test_tag_name()
   call assert_equal('foo-bar', call(TagName, ['</foo-bar>']))
   call assert_equal('foo-bar', call(TagName, ['<foo-bar />']))
 
+  call assert_equal('p', call(TagName, ['<p>']))
   call assert_equal('h1', call(TagName, ['<h1>']))
   call assert_equal('x1foo-y2bar', call(TagName, ['<x1foo-y2bar>']))
 endfunction